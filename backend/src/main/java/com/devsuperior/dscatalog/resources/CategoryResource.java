971232694a3e9075993cb3526d170c0e2d6853a5
julio <juliocesar13b@gmail.com>
2026-07-09 19:54:06 -0300

    adicionando banco e camadas

--- backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/CategoryResource.java
@@ -1,6 +1,7 @@
 package com.devsuperior.dscatalog.resources;
 
 import com.devsuperior.dscatalog.entites.Category;
+import com.devsuperior.dscatalog.services.CategoryService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,13 +14,15 @@ import java.util.List;
 @RequestMapping("/categories")
 public class CategoryResource {
 
+    private final CategoryService categoryService;
+
+    public CategoryResource(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 
     @GetMapping
     public ResponseEntity<List<Category>> findAll(){
-        List<Category> list = new ArrayList<>();
-        list.add(new Category(1L, "Books"));
-        list.add(new Category(2L, "Tables"));
-        list.add(new Category(2L, "Eletronics"));
+        List<Category> list = categoryService.findAll();
         return ResponseEntity.ok().body(list);
     }
 }
